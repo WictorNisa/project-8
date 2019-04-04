213e07f4683f328edc86048a73e9d351bcd66339
Blue Circus <wictornisa@hotmail.com>
2019-04-04 09:51:05 +0200

    appending diffrent images to same grid?

--- js/app.js
+++ js/app.js
@@ -1,6 +1,22 @@
-const img = document.getElementById('img-holder');
+const img = document.querySelector('.img-holder');
 const text = document.querySelector('#text-holder');
 
+let people = [];
 fetch('https://randomuser.me/api/?inc=name,picture,email,location,phone,dob&results=12')
-.then(response => response.json())
-.then(data => console.log(data))
+  .then(response => response.json())
+  .then(data => {
+    people = data.results;
+    console.log(people);
+  })
+  .then(data => generateImg(data));
+
+
+  function generateImg(data){
+    people.forEach((person) => {
+      const avatarImg = person.picture.large;
+      const html = `
+        <img src="${avatarImg}">
+      `;
+      img.innerHTML = html;
+    });
+}
