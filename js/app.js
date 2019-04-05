04194787f762f7e260da551013f171482bef15cb
Blue Circus <wictornisa@hotmail.com>
2019-04-05 19:58:36 +0200

    save

--- js/app.js
+++ js/app.js
@@ -1,22 +1,22 @@
-const img = document.querySelector('.img-holder');
+const imgHolder = document.querySelectorAll('.img-holder');
 const text = document.querySelector('#text-holder');
 
 let people = [];
 fetch('https://randomuser.me/api/?inc=name,picture,email,location,phone,dob&results=12')
   .then(response => response.json())
-  .then(data => {
-    people = data.results;
-    console.log(people);
-  })
+  .then (data => people = data.results)
   .then(data => generateImg(data));
 
 
-  function generateImg(data){
-    people.forEach((person) => {
-      const avatarImg = person.picture.large;
-      const html = `
-        <img src="${avatarImg}">
-      `;
-      img.innerHTML = html;
-    });
+function generateImg(data){
+  people.forEach((person) => {
+    const avatarImg = person.picture.large;
+    let avatar = document.createElement('img');
+    avatar.setAttribute('src', avatarImg);
+    let html = `
+      <img src="${avatar}">
+    `;
+
+    imgHolder.innerHTML = html;
+  });
 }
