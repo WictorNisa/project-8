b35e490992fe045232bd154839fcde91b5d05162
Blue Circus <wictornisa@hotmail.com>
2019-04-05 22:45:02 +0200

    finalized directory view

--- js/app.js
+++ js/app.js
@@ -1,11 +1,12 @@
 const imgHolder = document.querySelectorAll('.img-holder');
-const text = document.querySelector('#text-holder');
+const textHolder = document.querySelectorAll('.text-holder');
 
 let people = [];
 fetch('https://randomuser.me/api/?inc=name,picture,email,location,phone,dob&results=12')
   .then(response => response.json())
   .then (data => people = data.results)
-  .then(data => generateImg(data));
+  .then(data => generateImg(data))
+  .then(data => generateName(data));
 
 
 function generateImg(data){
@@ -14,7 +15,28 @@ function generateImg(data){
     let html = `
       <img src="${avatarImg}">
     `;
-
     imgHolder[index].innerHTML = html;
   });
+}
+
+function generateName(data) {
+  people.forEach((person, index) => {
+    const firstName = person.name.first;
+    const lastName = person.name.last;
+    const email = person.email;
+    const city = person.location.city;
+    let nameHtml = `
+      <p class="avatar-name">${firstName} ${lastName}</p>
+      <p class="avatar-email"> ${email}</p>
+      <p class="avatar-city"> ${city}</p>
+    `;
+    console.log(people);
+    textHolder[index].innerHTML = nameHtml;
+  });
+}
+
+
+function capFirst(string)
+{
+    return string.charAt(0).toUpperCase() + string.slice(1);
 }
