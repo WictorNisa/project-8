f522fe2c5229ffc063a65b7513e40524eead8d35
Blue Circus <wictornisa@hotmail.com>
2019-04-09 15:25:06 +0200

    problem with modal

--- js/app.js
+++ js/app.js
@@ -1,14 +1,19 @@
 const imgHolder = document.querySelectorAll('.img-holder');
 const textHolder = document.querySelectorAll('.text-holder');
 const modalView = document.querySelectorAll('.modal-view');
+const gridSelect = document.getElementById('grid-select');
+const closeX = document.getElementById('close');
+const modalBlock = document.querySelector('.modal-block');
+const modal = document.querySelector('.modal');
+
 
 let people = [];
 fetch('https://randomuser.me/api/?inc=name,picture,email,location,phone,dob&results=12')
   .then(response => response.json())
   .then (data => people = data.results)
   .then(data => generateImg(data))
-  .then(data => generateName(data))
-  .then(data => generateModalInfo(data));
+  .then(data => generateName(data));
+  // .then(data => generateModalInfo(data));
 
 
 function generateImg(data){
@@ -32,19 +37,49 @@ function generateName(data) {
       <p class="avatar-email"> ${email}</p>
       <p class="avatar-city"> ${city}</p>
     `;
-    console.log(people);
     textHolder[index].innerHTML = nameHtml;
   });
 }
 
-function generateModalInfo(data) {
+function generateModalInfo(index) {
   people.forEach((person, index) => {
     const cell = person.phone;
     const street = person.location.street;
     const city = person.location.city;
     const state = person.location.state;
     const postcode = person.location.postcode;
-    const bithday = person.dob.date;
-
+    const birthday = person.dob.date;
+    let infoHtml = `
+      <p>${cell}</p>
+      <p>${street} ${state} ${city} ${postcode}</p>
+      <p>Birthday: ${birthday}</p>
+    `;
+      modalView[index].innerHTML = infoHtml;
   });
 }
+
+function openModal(e) {
+  if(e.target.classList.contains('grid-hover')) {
+    modalBlock.style.display = 'flex';
+    modal.style.display = 'flex';
+    console.log('Connected');
+    }
+    if(e.target.matches(index)) {
+      generateModalInfo(index);
+      }
+    }
+
+
+
+function closeModal(e) {
+  if(e.target.classList.contains('close')) {
+    modal.style.display ='none';
+    modalBlock.style.display ='none';
+  }
+    return false;
+}
+
+
+gridSelect.addEventListener('click', openModal);
+
+closeX.addEventListener('click', closeModal);
