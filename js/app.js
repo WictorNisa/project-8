b8b8dac40355178c55b82c12f0df60d19e319ecf
Blue Circus <wictornisa@hotmail.com>
2019-04-10 21:56:07 +0200

    broke the code

--- js/app.js
+++ js/app.js
@@ -6,45 +6,17 @@ const gridSelect = document.getElementById('grid-select');
 const closeX = document.getElementById('close');
 // const modalBlock = document.querySelector('.modal-block');
 const modal = document.querySelector('.modal');
-
+const modalContainer = document.querySelector('#modal-text-container');
 
 let people = [];
 fetch('https://randomuser.me/api/?inc=name,picture,email,location,phone,dob&results=12')
   .then(response => response.json())
   .then (data => people = data.results)
-  .then(data => generateImg(data))
-  .then(data => generateName(data))
+  .then(data => generateCard(data))
   .then(data => generateModalInfo(data));
 
-
-function generateImg(data){
-  people.forEach((person, index) => {
-    const avatarImg = person.picture.large;
-    let html = `
-      <img src="${avatarImg}">
-    `;
-    imgHolder[index].innerHTML = html;
-  });
-}
-
-function generateName(data) {
-  people.forEach((person, index) => {
-    const firstName = person.name.first;
-    const lastName = person.name.last;
-    const email = person.email;
-    const city = person.location.city;
-    let nameHtml = `
-      <p class="avatar-name">${firstName} ${lastName}</p>
-      <p class="avatar-email"> ${email}</p>
-      <p class="avatar-city"> ${city}</p>
-    `;
-    textHolder[index].innerHTML = nameHtml;
-  });
-}
-
-function generateModalInfo(data) {
-  let people =[];
-  for(let i = 0; i < people.length; i++) {
+  function generateModalInfo(data) {
+  people.forEach((person, index)) => {
     const avatarImg = person.picture.large;
     const firstName = person.name.first;
     const lastName = person.name.last;
@@ -56,29 +28,55 @@ function generateModalInfo(data) {
     const postcode = person.location.postcode;
     const birthday = person.dob.date;
       let infoHtml = `
+      <div class="modal-text">
         <img class="modal-avatar" src="${avatarImg}">
         <p>${firstName} ${lastName}</P>
         <p>${email}</p>
         <p>${cell}</p>
         <p>${street} ${state} ${city} ${postcode}</p>
         <p>Birthday: ${birthday}</p>
+      </div>
       `;
-        modalViewer[i].innerHTML = infoHtml;
+        modalContainer.innerHTML += infoHtml;
+  }
+}
+
+function generateModalInfo(data) {
+  people.forEach((person, index)) => {
+    const avatarImg = person.picture.large;
+    const firstName = person.name.first;
+    const lastName = person.name.last;
+    const email = person.email;
+    const cell = person.phone;
+    const street = person.location.street;
+    const city = person.location.city;
+    const state = person.location.state;
+    const postcode = person.location.postcode;
+    const birthday = person.dob.date;
+      let infoHtml = `
+      <div class="modal-text">
+        <img class="modal-avatar" src="${avatarImg}">
+        <p>${firstName} ${lastName}</P>
+        <p>${email}</p>
+        <p>${cell}</p>
+        <p>${street} ${state} ${city} ${postcode}</p>
+        <p>Birthday: ${birthday}</p>
+      </div>
+      `;
+        modalContainer.innerHTML += infoHtml;
   }
 }
 
 function openModal(e) {
-  let gridContain = e.target.closest('div.grid-contain');
-  if(gridContain) {
-    modalBlock.style.display = 'flex';
+  const modalText = modalContainer.querySelectorAll('.modal-text');
+  const gridContain = e.target.closest('div.grid-contain');
+  if(gridContain){
     modal.style.display = 'flex';
-    console.log(e.target);
+    console.log(gridContain.getAttribute('data-index'));
     return;
       }
-    // if(e.target.) {
-    //
-    // }
     }
+
 
 
 
